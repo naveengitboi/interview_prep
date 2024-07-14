3ec3e421957fff825406e93d5db73b90794c8dc4
naveengitboi <naveenjangiti9@gmail.com>
2024-07-14 19:31:25 +0530

    added core js

--- src/coreJs/Conceptual.tsx
+++ src/coreJs/Conceptual.tsx
@@ -31,7 +31,7 @@ function duplicateArray(arr: number[]) {
   return [...arr, ...arr]
 }
 
-console.log('duplicate give array', duplicateArray([1, 2, 3, 4, 5, 6]))
+// console.log('duplicate give array', duplicateArray([1, 2, 3, 4, 5, 6]))
 
 
 const CoreConcepts = () => {
