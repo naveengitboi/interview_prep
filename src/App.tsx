e6d25a2239bfb7bfec43de4191b1ddc627bda7ab
naveengitboi <naveenjangiti9@gmail.com>
2024-07-14 19:20:50 +0530

    Core js concepts

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import './App.css'
-import InputDelay from './components/InputDelay'
+
+// import InputDelay from './components/InputDelay'
+// import Pagination from './components/Pagination'
 // import Search from './components/Search'
+// import DisableBtn from './components/DisableBtn.tsx'
+// import QuizApp from './components/QuizApp.tsx'
+import CoreConcepts from './coreJs/Conceptual.tsx'
 
 function App() {
 
@@ -8,8 +13,11 @@ function App() {
     <div className='app'>
       <h1 className="mainH1">Interview prep</h1>
       {/* <Search /> */}
-      <InputDelay />
-
+      {/* <InputDelay /> */}
+      {/* <Pagination /> */}
+      {/* <DisableBtn/> */}
+      {/* <QuizApp /> */}
+      <CoreConcepts />
     </div>
 
   )
